f188728c2d824569b6043423dceb2eda7794287c
Jesse Follet <jesse.follet@yahoo.com>
2017-05-11 01:41:02 +0000

    Changed Entry to cls in the classmethods function in Entry model

--- learning_journal/models/entry.py
+++ learning_journal/models/entry.py
@@ -11,24 +11,25 @@ import datetime
 
 from .meta import Base
 
+import sqlalchemy as sa
 
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
     title = Column(Unicode(255), nullable=False)
-    body = Column(UnicodeText, nullable=True)
+    body = Column(UnicodeText, default="", nullable=True)
     created = Column(DateTime, default=datetime.datetime.utcnow())
     edited = Column(DateTime,  default=datetime.datetime.utcnow())
 
     @classmethod
     def all(cls, session):
         """Return all"""
-        return session.query(Entry).order_by(desc(Entry.created))
+        return session.query(cls).order_by(desc(cls.created))
 
     @classmethod
     def by_id(cls, session, id):
         """Return entry by id number"""
-        return session.query(Entry).get(id)
+        return session.query(cls).get(id)
 
 
 # Index('my_index', Entry.title, unique=True, mysql_length=255)
